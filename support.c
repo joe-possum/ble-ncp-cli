6ca3605c5c4bd68d675ab9cedc06efd707e36b15
Mark Orchard-Webb <Mark.Orchard-Webb@silabs.com>
2019-12-14 15:04:48 -0600

    Creation of ble-ncp-cli

---
+++ support.c
@@ -0,0 +1,10 @@
+#include <time.h>
+
+void millisleep(uint32_t ms) {
+  struct timespec remaining, duration;
+  duration.tv_sec = ms / 1000;
+  duration.tv_nsec = (ms - 1000*duration.tv_sec) * 1000000;
+  do {
+    nanosleep(&duration,&remaining);
+  } while (remaining.tv_sec || remaining.tv_nsec);
+}
